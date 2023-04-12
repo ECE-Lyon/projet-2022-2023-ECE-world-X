2808b1067d00c49e113ec123dcf03356c6338b6d
Tristan <tristan@tristanjean.com>
2023-04-12 19:33:13 +0200

    Update structure et test direction

---
+++ plateau.h
@@ -0,0 +1,16 @@
+//
+// Created by trist on 12/04/2023.
+//
+
+#ifndef PROJET_PLATEAU_H
+#define PROJET_PLATEAU_H
+
+typedef struct {
+    int widthsquare, heightsquare;
+    int nb;
+}Damier;
+
+void init_board(Damier* board);
+
+
+#endif //PROJET_PLATEAU_H
