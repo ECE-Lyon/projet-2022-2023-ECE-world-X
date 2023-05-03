aeb94309fa5b749c50d602da6411fb586bb734f7
Tristan <tristan@tristanjean.com>
2023-05-03 10:23:43 +0200

    debug fin define "MAXCHAR"

--- Map/character.h
+++ Map/character.h
@@ -13,7 +13,7 @@ enum{B1, B2, B3, L1, L2, L3, R1, R2, R3, T1, T2, T3}; //B = bot, L = Left, R = R
 
 
 typedef struct{
-    char name[MAXCHAR];
+    char name[MAXCH];
     int x,y, direction, state, ticket, side;
     ALLEGRO_BITMAP* anim[NBANIMATION];
     ALLEGRO_BITMAP* img;
